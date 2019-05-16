bee73bece03201ba22c889eb44e770cc73353e83
mmshabab90 <mmshabab90@gmail.com>
2019-05-16 20:28:37 +0200

    added another page to test for site redirection in git

---
+++ jsdemo1/js/main.js
@@ -0,0 +1,15 @@
+var wordToDisplay = "Hello Again...";
+
+var red = [0, 100, 63];
+var green = [75, 100, 40];
+var blue = [197, 77, 55];
+var yellow = [255, 250, 12];
+
+var letterColors = [red, green, blue, yellow];
+
+drawName(wordToDisplay, letterColors);
+
+bubbleShape = 'circle';
+
+bounceBubbles();
+bounceName();
